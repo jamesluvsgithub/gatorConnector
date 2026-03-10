7c17cb2f2ce27eaa3a4b42f25da284394d1728c1
James chau <jchau509@gmail.com>
2026-03-10 18:39:13 -0400

    Added User.js

---
+++ backend/src/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+
+        password: {
+            type: String,
+            required: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+
+        bio: {
+            type: String,
+            default: ""
+        },
+
+        friends: [{
+            
+        }],
+
+        friendRequestsSent: [{
+            
+        }],
+
+        friendRequestsReceived: [{
+            
+        }]
+    }
+)

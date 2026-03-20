b2abd1154accd2945d20e6349430500fd542b3d9
ljsteff <leah.steffer@ufl.edu>
2026-03-20 16:20:57 -0400

    Updating MONGO variable to be more descriptive and deleting env.txt file.

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.json({msg: 'Welcome to gatorConnectors'})
 })
 
-mongoose.connect(process.env.MONGO)
+mongoose.connect(process.env.MONGO_DB_URI)
     .then(() => {
         app.listen(process.env.PORT, () => {
             console.log('listening on port')

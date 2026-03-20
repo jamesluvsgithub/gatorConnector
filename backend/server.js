526653aabad272d989138fcdce2ede447dde7b21
James chau <jchau509@gmail.com>
2026-03-20 02:16:28 -0400

    Added Create and Read controllers

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const express = require('express')
 
 const app = express()
 
+mongoose.connect(process.env.MONGO_DB_URI)
+    .then(() => console.log("Connected to MongoDB!"))
+    .catch(err => console.error(err))
+
 app.get('/', (req, res) => {
     res.json({msg: 'Welcome to gatorConnectors'})
 })

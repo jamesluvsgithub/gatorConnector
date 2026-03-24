8e419c1e14363222b6f711022dfe8f6932aff00c
Chang <cchang529@outlook.com>
2026-03-23 20:53:03 -0400

    update app view for testing components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,12 @@
 import "./App.css";
 import Login from "./components/Login";
 import GoalWindow from "./components/GoalWindow";
+import ChatWindow from "./components/ChatWindow";
 
 function App() {
-  const showGoal = true;
-
-  return showGoal ? <GoalWindow /> : <Login />;
+  // return <GoalWindow />;
+  return <ChatWindow />;
+  // return <Login />;
 }
 
 export default App;

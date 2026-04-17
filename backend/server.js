20b834ff457650a57a14174e176af910a592e515
ljsteff <leah.steffer@ufl.edu>
2026-04-17 15:33:10 -0400

    Added server functionality for the matching algorithm. Also updated the user routes to account for new signup feature.

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,14 @@ const goalRoutes = require("./src/routes/goalRoutes");
 const facultyRoutes = require("./src/routes/facultyRoutes");
 const chatRoutes = require("./src/routes/chatRoutes");
 const messageRoutes = require("./src/routes/messageRoutes");
+const matchingRoutes = require("./src/routes/matchingRoutes");
 
 app.use("/api/users", userRoutes);
 app.use("/api/goals", goalRoutes);
-app.use("/api/goals", facultyRoutes);
-app.use("/api/goals", chatRoutes);
-app.use("/api/goals", messageRoutes);
+app.use("/api/faculty", facultyRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/matching", require("./src/routes/matchingRoutes"));
 
 mongoose.connect(process.env.MONGO_DB_URI)
   .then(() => console.log("Connected to MongoDB"))
